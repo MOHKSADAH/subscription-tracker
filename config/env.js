0570ca2d8b44172f66980ade357e710b26b22e4d
Mohamad Al-Sadah <Mideaxinq@gmail.com>
2025-09-05 09:35:04 +0300

    added & implemented arcjet functionality

--- config/env.js
+++ config/env.js
@@ -2,4 +2,5 @@ import { config } from 'dotenv';
 // switching between production and development without overriding one another
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_KEY, ARCJET_ENV } =
+    process.env;
